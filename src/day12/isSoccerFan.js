d8773ae676a49b58e3ccc45b0e204957724a4ed9
Akin Okegbile <aokegbile@gmail.com>
2017-10-08 20:55:58 -0500

    This is sample commit message

--- src/day12/isSoccerFan.js
+++
@@ -1,5 +0,0 @@
-let isSoccerFan =false;
-if(isSoccerFan){
-  console.log('Goal!');
-}
-else{ console.log('No goal!')}
--- src/day13/starCount.js
+++
@@ -1,9 +0,0 @@
-const starCount = () =>{
-  let i = 5;
-  console.log(i);
-  for (i=0;i<12;i++){
-    console.log(i);
-  }
-}
-starCount();
-console.log(i)
