b2876a2869dfe54f83e7ca8fc685f0a63842009a
Akin Okegbile <aokegbile@gmail.com>
2017-10-08 21:33:10 -0500

    Feature Add

--- src/airplane.js
+++ src/airplane.js
@@ -1,44 +1,38 @@
-let
-  availableAirplanes= [
-    {
-      name: 'AeroJet',
-      fuelCapacity:800,
-      availableStaff:[
-        'pilots',
-        'flightAttendants',
-        'engineers',
-        'medicalAssistance',
-        'sensorOperators'
-      ],
-      maxSpeed:1200,
-      minSpeed: 300
-    },
-    {
-      name: 'SkyJet',
-      fuelCapacity:500,
-      availableStaff:[
-        'pilots',
-        'flightAttendants',
-      ],
-      maxSpeed: 800,
-      minSpeed: 200
-    }
-  ]
-let flightRequirements = {
+export let availableAirplanes = [
+{name: 'AeroJet',
+ fuelCapacity: 800,
+ availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+ maxSpeed: 1200,
+ minSpeed: 300
+},
+{name: 'SkyJet',
+ fuelCapacity: 500,
+ availableStaff: ['pilots', 'flightAttendants'],
+ maxSpeed: 800,
+ minSpeed: 200
+}
+];
+
+export let flightRequirements = {
   requiredStaff: 4,
-  requiredSpeedRange:700
+  requiredSpeedRange: 700
 };
-function meetsSpeedRangeRequirements(maxSpeed,minSpeed,requiredSpeedRange){
-  const range =maxSpeed-minSpeed;
-  if(requiredSpeedRange<range){
+
+export function meetsStaffRequirements(availableStaff, requiredStaff) {
+  if (availableStaff.length > requiredStaff) {
     return true;
+  } else {
+    return false;
   }
-  return false;
-}
-const meetsStaffRequirements = (availableStaff,requiredStaff)=>{
- 	if(availableStaff.length >requiredStaff){
+};
+
+export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
+  let range = maxSpeed - minSpeed;
+  if (range > requiredSpeedRange) {
     return true;
+    } else {
+    return false;
   }
-  return false;
-}
-export{availableAirplanes as aircrafts,flightRequirements as flightReqs, meetsStaffRequirements as meetsStaffReqs, meetsSpeedRangeRequirements as meetsSpeedRangeReqs};
+};
+
+export default meetsSpeedRangeRequirements;
