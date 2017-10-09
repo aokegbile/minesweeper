071aab8fc82f5bacb2b8c66f5ff548c2ce7c6b90
Akin Okegbile <aokegbile@gmail.com>
2017-10-08 21:05:35 -0500

    New Files Added.

---
+++ src/1-missionControl.js
@@ -0,0 +1,7 @@
+const Airplane = require('./1-airplane.js');
+
+const displayAirplane = ()=>{
+  console.log(Airplane.myAirplane);
+}
+
+displayAirplane();
---
+++ src/airplane.js
@@ -0,0 +1,6 @@
+let Airplane = {
+  availableAirplanes: [
+    {name: 'AeroJet',fuelCapacity:800},			{name: 'SkyJet', fuelCapacity:500}
+  ]
+}
+export default Airplane;
