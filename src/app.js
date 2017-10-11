445482e23bec147b97ef5909698adf0f8bdd3dd3
Akin Okegbile <aokegbile@gmail.com>
2017-10-10 23:58:06 -0500

    Update

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+let myList = (
+  <ul>
+    <li>all</li>
+
+    <li>tab</li>
+
+    <li>new</li>
+  </ul>);
+ReactDOM.render(myList,document.getElementById('app'));
